bff20f30a575d211c1494fc1474f3c0edc2a2bb2
James <jselby@jselby.net>
2018-01-31 18:47:44 +1100

    Add OpenSSL dependency to Travis, cache Cargo artifacts

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,9 +2,11 @@
 cd /liftinstall
 
 apt update
-apt install -y libwebkit2gtk-4.0-dev
+apt install -y libwebkit2gtk-4.0-dev libssl-dev
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH=~/.cargo/bin:$PATH
 
 cargo build
+
+cp -r ~/.cargo/ ./.cargo/

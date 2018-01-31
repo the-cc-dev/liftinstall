14590b2df6da8b140b98a23c3c74ec5160b3b187
James <jselby@jselby.net>
2018-01-31 18:37:25 +1100

    Accept defaults for Rustup

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,7 +4,7 @@ cd /liftinstall
 apt update
 apt install -y libwebkit2gtk-4.0-dev
 
-curl https://sh.rustup.rs -sSf | sh -s
+curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH=~/.cargo/bin:$PATH
 
 cargo build

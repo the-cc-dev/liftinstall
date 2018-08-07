708a194764496923905409f32fc80e445452e9d7
James <jselby@jselby.net>
2018-08-07 15:48:49 +1000

    Fix launcher with new config system

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -26,7 +26,7 @@ function ajax(path, successCallback, failCallback, data) {
         if (this.status === 200 && this.getResponseHeader('Content-Type').indexOf("application/json") !== -1) {
             successCallback(JSON.parse(this.responseText));
         } else {
-            failCallback();
+            failCallback(this.responseText);
         }
     });
     req.addEventListener("error", failCallback);
@@ -69,7 +69,7 @@ function stream_ajax(path, callback, successCallback, failCallback, data) {
         if (this.status === 200) {
             successCallback(this.responseText);
         } else {
-            failCallback();
+            failCallback(this.responseText);
         }
     });
 
